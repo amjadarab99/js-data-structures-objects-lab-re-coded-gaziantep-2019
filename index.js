c7ba9d8d83c4acf2991ae33659d3c1ed200f5394
Amjad Arab <amjad.arab99@gmail.com>
2019-07-24 21:08:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
  driver[key] = value;
  return driver;
 }
+function deleteFromDriverByKey(driver,key){
+ const newObject = Object.assign({},driver);
+ delete newObject[key];
+ return newObject;
+}
+function destructivelyDeleteFromDriverByKey(driver,key){
+ delete driver[key];
+ return driver;
+}
